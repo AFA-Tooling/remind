a33e8448a833aa921f05e22aa30db479a31a233a
Ankita Sundar <ankitasun@berkeley.edu>
2024-09-30 20:56:38 -0700

    adding student assignments and due dates

--- remind/my-react-app/src/backend.js
+++ remind/my-react-app/src/backend.js
@@ -1,21 +1,3 @@
-/*function studentList() {
-    const students = [
-        { name: 'John Doe', assignment: 'HW 1', assignmentDate: new Date},
-        { name: 'Jane Smith', assignment: 'Lab 1', assignmentDate: new Date},
-        { name: 'Bob Johnson', assignment: 'Project 1', assignmentDate: new Date}
-    ];
-
-    return students.map((student) => {
-        return (
-            <div key={student.name}>
-                <h2>{student.name}</h2>
-                <p>Assignment: {student.assignment}</p>
-                <p>Assignment Date: {student.assignmentDate}</p>
-            </div>
-        );
-    });
-}*/
-
 function getAssignments() {
     const assignments = [
         { title: 'Project 1', assignedDate: '2024-09-03', dueDate: '2024-09-10' },
